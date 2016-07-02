9f17cce5464d2d7ae9ca87e26b0345718ef6c851
nlct <n.talbot@uea.ac.uk>
2016-07-02 14:26:32 +0100

    Fixed argument test

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # texosquery
 cross-platform Java application to query OS information designed for use in TeX's shell escape mechanism
+
+Example useage:
+
+
+```latex
+% arara: pdflatex: {shell: on}
+\documentclass{article}
+
+\begin{document}
+Test document.
+
+\ttfamily
+
+\input{|"texosquery --locale"}
+
+\input{|"texosquery --pdfnow"}
+
+\input{|"texosquery --pdfdate \jobname.tex"}
+
+
+\end{document}
+```
--- src/java/TeXOSQuery.java
+++ src/java/TeXOSQuery.java
@@ -259,7 +259,7 @@ public class TeXOSQuery
          {
             i++;
 
-            if (i < args.length)
+            if (i >= args.length)
             {
                System.err.println(
                  String.format("filename expected after %s", args[i-1]));
@@ -272,7 +272,7 @@ public class TeXOSQuery
          {
             i++;
 
-            if (i < args.length)
+            if (i >= args.length)
             {
                System.err.println(
                  String.format("filename expected after %s", args[i-1]));
