2184847d2e2b7d115e4b085331e19883a085a735
nlct <n.talbot@uea.ac.uk>
2016-11-24 10:23:40 +0000

    Corrected bug in month number (-M/--date-time)

--- src/java/TeXOSQuery.java
+++ src/java/TeXOSQuery.java
@@ -837,7 +837,7 @@ public class TeXOSQuery implements Serializable
        cal.get(Calendar.ERA),
        cal.get(Calendar.YEAR),
        getWeekYear(cal),
-       cal.get(Calendar.MONTH),
+       cal.get(Calendar.MONTH)+1,
        cal.get(Calendar.WEEK_OF_YEAR),
        cal.get(Calendar.WEEK_OF_MONTH),
        cal.get(Calendar.DAY_OF_YEAR),
